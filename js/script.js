533c71bc4810d2d7cd7f517e70314941fcc9e22a
Oliwier Kotlicki <oliwier.kotlicki@gmail.com>
2023-11-07 09:05:14 +0100

    początek programu

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const userFirstNumber = document.querySelector(".firstNumber");
+const userSecondNumber = document.querySelector(".secondNumber");
+const userThirdNumber = document.querySelector(".thirdNumber");
+const userFourthNumber = document.querySelector(".fourthNumber");
+const userFifthNumber = document.querySelector(".fifthNumber");
+const userSixthNumber = document.querySelector(".sixthNumber");
